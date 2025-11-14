1d9304705b05c1604b97a5c0b4cb0ee72fd0d2da
Juanjo3472 <juanjose_cg@outlook.com>
2025-11-13 21:27:53 -0600

    Update square_flight.py

--- square_flight.py
+++ square_flight.py
@@ -1,57 +1,45 @@
 import robomaster
+import time 
 from robomaster import robot
-import cv2
-import numpy as np
-
-def nothing(x):
-    pass
-
-# Crear ventana de sliders
-cv2.namedWindow("Sliders")
-cv2.createTrackbar("H min", "Sliders", 80, 179, nothing)
-cv2.createTrackbar("H max", "Sliders", 100, 179, nothing)
-cv2.createTrackbar("S min", "Sliders", 100, 255, nothing)
-cv2.createTrackbar("S max", "Sliders", 255, 255, nothing)
-cv2.createTrackbar("V min", "Sliders", 100, 255, nothing)
-cv2.createTrackbar("V max", "Sliders", 255, 255, nothing)
 
 # Inicializar dron
 tl_drone = robot.Drone()
 tl_drone.initialize()
-camera = tl_drone.camera
-camera.start_video_stream(display=False)
 
-try:
-    while True:
-        frame = camera.read_cv2_image(strategy="newest", timeout=5)
-        frame = cv2.resize(frame, (640, 480))
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Leer valores HSV de sliders
-        h_min = cv2.getTrackbarPos("H min", "Sliders")
-        h_max = cv2.getTrackbarPos("H max", "Sliders")
-        s_min = cv2.getTrackbarPos("S min", "Sliders")
-        s_max = cv2.getTrackbarPos("S max", "Sliders")
-        v_min = cv2.getTrackbarPos("V min", "Sliders")
-        v_max = cv2.getTrackbarPos("V max", "Sliders")
+if __name__ == '__main__':
 
-        lower = np.array([h_min, s_min, v_min])
-        upper = np.array([h_max, s_max, v_max])
+    tl_flight = tl_drone.flight
 
-        mask = cv2.inRange(hsv, lower, upper)
-        result = cv2.bitwise_and(frame, frame, mask=mask)
+    # Set the QUAV to takeoff
+    tl_flight.takeoff().wait_for_completed()
 
-        # Mostrar imágenes
-        cv2.imshow("Original", frame)
-        cv2.imshow("Mascara", mask)
-        cv2.imshow("Resultado", result)
+    # Add a delay to remain in hover
+    print("Remaning in hover")
+    time.sleep(5)
 
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+    #tl_flight.forward(distance=60).wait_for_completed()
 
-finally:
-    camera.stop_video_stream()
+    tl_flight.down(distance=30).wait_for_completed()
+
+    for i in range(2):
+        for j in range(7):
+            tl_flight.forward(distance=60).wait_for_completed()
+            if j % 2 == 0:
+                tl_flight.right(distance=10).wait_for_completed()
+            time.sleep(1)
+        tl_flight.rotate(angle=90).wait_for_completed()
+        time.sleep(1)
+        for k in range(6):
+            tl_flight.forward(distance=60).wait_for_completed()
+            if k % 2 == 0:
+                tl_flight.right(distance=10).wait_for_completed()
+            time.sleep(1)
+        tl_flight.rotate(angle=90).wait_for_completed()
+        time.sleep(1)
+
+    # Set the QUAV to land
+    tl_flight.land().wait_for_completed()
+
+    # Close resources
     tl_drone.close()
-    cv2.destroyAllWindows()
-    print("Dron desconectado")
